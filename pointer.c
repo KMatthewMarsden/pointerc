816716a8a5a6a34be739428ab2f80335d8c96084
Marsden <marsden8@egb070000039.lancs.local>
2019-11-12 10:19:57 +0000

    Added comments

--- pointer.c
+++ pointer.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-void f(int *j) {
-  (*j)++;
+void f(int *j) {   //Passing address of i by "reference" to the variable (though this is actually pass by value)
+  (*j)++;           //Increment the value of "i" by 1 
 }
 
-int main() {
-  int i = 20;
-  int *p = &i;
-  f(p);
-  printf("i = %d\n", i);
+int main() {        
+  int i = 20;       //Integer declared
+  int *p = &i;      //Pointer *p declared, points to the address (&) of "i"
+  f(p);                     //Pass the pointer to the function
+  printf("i = %d\n", i);    //Print! Should be "21" instead of "20" 
 
   return 0;
 }
